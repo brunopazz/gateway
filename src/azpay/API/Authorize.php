e427bd3fa17db017adf1e1d38ca1abae9df4d16e
Bruno Paz <brunopaz@azclick.com.br>
2018-12-27 14:06:38 -0200

    First Commit

--- src/azpay/API/Authorize.php
+++ src/azpay/API/Authorize.php
@@ -25,19 +25,14 @@
         }
 
 
-        /**
-         * @return mixed
-         */
+
         public function getJsonRequest()
         {
             return $this->jsonRequest;
         }
 
-        /**
-         * @param mixed $jsonRequest
-         * @return Authorize
-         */
-        public function setJsonRequest($transaction)
+
+        public function setJsonRequest(Transaction $transaction)
         {
 
             $json["transaction-request"] = [
@@ -60,9 +55,7 @@
         {
             return json_encode($this->jsonRequest, JSON_PRETTY_PRINT);
         }
-        /**
-         * @return array|mixed
-         */
+
         public function jsonSerialize()
         {
             return get_object_vars($this);
